d9529bc40238eb57b180e4bd6762fd572e2aa5ca
liuyu00 <liuyu009x@163.com>
2022-03-09 16:49:49 +0800

    开发、tbas公共组件

--- document/git.md
+++ document/git.md
@@ -5,3 +5,10 @@
 
 1, 使用git的第一步就是新建一个git仓库 
 在项目目录中运行 `git init` 命令
+ 
+git add .  将没有添加到仓库的文件添加到仓库
+git add filePath 将指定文件添加到仓库
+
+git commit -m '提交说明'  用命令git commit告诉Git，把文件提交到仓库
+
+git status  查看更改
--- src/components/TabBar/index.vue
+++ src/components/TabBar/index.vue
@@ -1,15 +1,36 @@
 <template>
-  <div>
-
+  <div class="tabs">
+    <nav>
+      <a
+        v-for="(item, index) in tabs"
+        :key="index"
+        href="javascript:;"
+      >{{item.text}}</a>
+    </nav>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'tab-bar'
+  name: 'tab-bar',
+  props: {
+    tabs: Array
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.tabs {
+  width: 100%;
+  nav {
+    display: flex;
+    a {
+      padding: 0 10px;
+      line-height: 40px;
+      font-size: 14px;
+      text-decoration: none;
+      color: #333;
+    }
+  }
+}
 </style>
